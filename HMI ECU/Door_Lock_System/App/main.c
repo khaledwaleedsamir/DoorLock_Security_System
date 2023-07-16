047a219c817fceb3a35da383a349d7fd2ea71f41
Khaled Waleed <104536807+khaledwaleedsamir@users.noreply.github.com>
2023-07-16 13:23:41 +0300

    HMI: HAL update.
    Updated HAL drivers for HMI microcontroller.

--- HMI ECU/Door_Lock_System/App/main.c
+++ HMI ECU/Door_Lock_System/App/main.c
@@ -6,45 +6,23 @@
  */ 
 
 //#include <avr/io.h>
-#include "Leds.h"
-#include "PushButtons.h"
 #include "Lcd.h"
 #include "Keypad.h"
-#include "Buzzer.h"
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
 
 int main(void)
 {
-	H_Leds_Init();
-	H_PushButtons_Init(PUSH_BUTTON2);
-	H_PushButtons_Init(PUSH_BUTTON1);
+	
 	H_Lcd_Init();
-	//H_Keypad_Init();
-	H_Buzzer_Init();
 	H_Lcd_WriteString("Init Done !");
 
 	
     /* Replace with your application code */
     while (1) 
     {    
-		if(H_PushButtons_Read(PUSH_BUTTON1) == PRESSED)
-		{
-			H_Lcd_Clear();
-			H_Leds_On(LED0);
-			_delay_ms(100);
-			H_Leds_Off(LED0);
-		}
-		if(H_PushButtons_Read(PUSH_BUTTON2) == PRESSED)
-		{
-			H_Buzzer_On();
-			_delay_ms(1000);
-			H_Buzzer_Off();
-		}
-		
-		
-		
+			
     }
 }
 
